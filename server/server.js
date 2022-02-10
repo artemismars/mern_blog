0c588959ad2b3e707f621b1dfe32d00f3626099f
artemismars <artemismars@outlook.com>
2022-02-10 21:00:37 +0900

    개발자 및 프로덕션 모드 설정 코드

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
+// dotenv 모듈을 이용한 환경변수 설정
+require("dotenv").config();
+// DB 연결 실행
+require("./db/connect");
+// 프로덕션 모드 yes or no
+if (process.env.NODE_ENV === "production") require("./config/production");
+console.log(`${process.env.NODE_ENV} mode on`);
+
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`listening to port on ${PORT}`);
