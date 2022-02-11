855d6761da3ecebb355e52f542646bc185592fc1
Taewook <rhkrxodnr@gmail.com>
2022-02-11 11:33:24 +0900

    add router dom connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-import './App.css'
+import Signup from './pages/Signup/signup'
+import Login from './pages/Login/login'
+import Dashboard from './pages/Dashboard/dashboard'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <p>초기세팅</p>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
 
