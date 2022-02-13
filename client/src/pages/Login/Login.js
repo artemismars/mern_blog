78e772ec57f1b46910b067d1acf50b3300436fb5
Taewook <rhkrxodnr@gmail.com>
2022-02-13 17:01:28 +0900

    mark up HTML [Login]

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -3,26 +3,20 @@ import styles from './login.module.css'
 function Login(props) {
   return (
     <div className={styles.container}>
-      <h1 className={styles.header}>로그인</h1>
       <form className={styles.loginForm}>
-        <div className={styles.emailContainer}>
-          <label className={styles.emailLabel} htmlFor="">
-            이메일
-          </label>
-          <input className={styles.emailInput} type="text" />
-        </div>
-        <div className={styles.passwordContainer}>
-          <label className={styles.passwordLabel} htmlFor="">
-            비밀번호
-          </label>
-          <input className={styles.passwordInput} type="text" />
-        </div>
+        <h1 className={styles.header}>로그인</h1>
+        <input className={styles.emailInput} placeholder="Email" type="text" />
+        <input
+          className={styles.passwordInput}
+          placeholder="Password"
+          type="text"
+        />
         <button className={styles.loginBtn}>로그인</button>
+        <div className={styles.signupContainer}>
+          <span className={styles.signupSpan1}>아직 계정이 없으신가요?</span>
+          <span className={styles.signupSpan2}>회원가입</span>
+        </div>
       </form>
-      <div className={styles.signupContainer}>
-        <span className={styles.signupSpan1}>아직 계정이 없으신가요?</span>
-        <span className={styles.signupSpan2}>회원가입</span>
-      </div>
     </div>
   )
 }
