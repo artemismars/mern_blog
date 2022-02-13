b4854ad9b78c1ff80505f2924e824f3674837a97
Donghyun Lee <skyblue5030@naver.com>
2022-02-13 23:12:58 +0900

    mark up signup page

--- client/src/pages/Signup/signup.js
+++ client/src/pages/Signup/signup.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import * as ROUTES from '../../constant/routes';
 
-const Signup = () => {
+import Styles from './signup.module.css';
+
+function Signup(props) {
+  const [userForm, setUserForm] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const onSubmitSignup = e => {
+    // 로직 작성..
+  }
+
   return (
-    <div>
-      <p>Sign up</p>
+    <div className={Styles.container}>
+      <form className={Styles.signupForm}>
+        <h1 className={Styles.header}>회원가입</h1>
+        <input
+          type="text"
+          name="usrname"
+          className={Styles.emailInput}
+          placeholder="Username"
+        />
+        <input
+          type="text"
+          name="email"
+          className={Styles.emailInput}
+          placeholder="Email"
+        />
+        <input
+          className={Styles.passwordInput}
+          placeholder="Password"
+          type="text"
+          name="password"
+        />
+        <button className={Styles.signupBtn} onClick={onSubmitSignup}>가입하기</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
