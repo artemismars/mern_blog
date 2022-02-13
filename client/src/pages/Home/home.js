23b732eb6826dcd499c154d167bd903867b0b263
Taewook <rhkrxodnr@gmail.com>
2022-02-13 14:43:23 +0900

    markup HTML [Login]

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import styles from './home.module.css'
 const Home = () => {
-  return <div>HOME</div>
+  return (
+    <div className={styles.container}>
+      <button className={`${styles.loginBtn} ${styles.btn}`}>로그인</button>
+      <button className={`${styles.loginBtn} ${styles.btn}`}>회원가입</button>
+    </div>
+  )
 }
 
 export default Home
