4b7d93fcd3949efa91f12f2dc446930a7bb7b720
artemismars <artemismars@outlook.com>
2022-02-14 23:24:46 +0900

    회원가입 로그인 라우터 세팅

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(express.json());
+app.use("/api", require("./routes/routers"));
+
 app.listen(PORT, () => {
   console.log(`listening to port on ${PORT}`);
 });
