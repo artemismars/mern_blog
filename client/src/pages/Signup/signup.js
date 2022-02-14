a01bded0c38a3691ed3c964cd22113ac233adc43
Taewook <rhkrxodnr@gmail.com>
2022-02-15 00:33:23 +0900

    remark-up signup page

--- client/src/pages/Signup/signup.js
+++ client/src/pages/Signup/signup.js
@@ -1,46 +1,111 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import * as ROUTES from '../../constant/routes';
-
-import Styles from './signup.module.css';
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import * as ROUTES from '../../constant/routes'
+import { Form, Input, Button, Checkbox } from 'antd'
+import styles from './signup.module.css'
 
 function Signup(props) {
   const [userForm, setUserForm] = useState({
     username: '',
     email: '',
     password: '',
-  });
+  })
 
-  const onSubmitSignup = e => {
+  const onSubmitSignup = (e) => {
     // 로직 작성..
+  }
+  const onFinish = (values) => {
+    console.log('Success:', values)
+  }
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo)
   }
 
   return (
-    <div className={Styles.container}>
-      <form className={Styles.signupForm}>
-        <h1 className={Styles.header}>회원가입</h1>
-        <input
-          type="text"
-          name="usrname"
-          className={Styles.emailInput}
-          placeholder="Username"
-        />
-        <input
-          type="text"
+    <div className={styles.container}>
+      <Form
+        className={styles.form}
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <h1 className={styles.header}>회원가입</h1>
+        <Form.Item
+          className={styles.item}
           name="email"
-          className={Styles.emailInput}
-          placeholder="Email"
-        />
-        <input
-          className={Styles.passwordInput}
-          placeholder="Password"
-          type="text"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your email!',
+            },
+          ]}
+        >
+          <Input className={styles.emailInput} placeholder="이메일" />
+        </Form.Item>
+        <Form.Item
+          className={styles.item}
           name="password"
-        />
-        <button className={Styles.signupBtn} onClick={onSubmitSignup}>가입하기</button>
-      </form>
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input className={styles.psInput} placeholder="비밀번호" />
+        </Form.Item>
+        <Form.Item
+          className={styles.item}
+          name="password2"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input className={styles.ps2Input} placeholder="비밀번호 확인" />
+        </Form.Item>
+        <Form.Item
+          className={styles.item}
+          name="nickname"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your nickname!',
+            },
+          ]}
+        >
+          <Input className={styles.nicknameInput} placeholder="닉네임" />
+        </Form.Item>
+        <Form.Item
+          className={styles.item}
+          name="blogname"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your blog name!',
+            },
+          ]}
+        >
+          <Input className={styles.blognameInput} placeholder="블로그 이름" />
+        </Form.Item>
+        <Button className={styles.signupBtn} type="primary" htmlType="submit">
+          회원가입
+        </Button>
+      </Form>
     </div>
-  );
+  )
 }
 
-export default Signup;
+export default Signup
