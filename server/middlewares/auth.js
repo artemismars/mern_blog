b4e3d309c1fd9adaf52d851aedb41b1dd7f02cbf
artemismars <artemismars@outlook.com>
2022-02-15 23:17:30 +0900

    Add 이메일 인증링크 전송 미들웨어
    nodemailer 모듈을 사용하여 이메일 인증 링크를 유저 이메일로 전송합니다.
    추가된 미들웨어 목록
    - `sendEmailVerification`: 관리자 gmail 계정을 이용하여 유저에게 인증링크를 전송합니다.
    - `verifyUser` : 이메일 인증 링크 접속시 유효한 인증링크인지 체크합니다.

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,6 +1,7 @@
 console.log(`auth working`);
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
 const User = require("../models/user");
 
 const generateConfirmationCode = async function (req, res, next) {
@@ -26,6 +27,49 @@ const generateConfirmationCode = async function (req, res, next) {
   }
 };
 
+const sendEmailVerification = (req, res, next) => {
+  const transport = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.SECRET_USER,
+      pass: process.env.SECRET_PASSWORD,
+    },
+  });
+
+  transport
+    .sendMail({
+      from: process.env.SECRET_USER,
+      to: res.locals.user.email,
+      subject: "회원가입을 위한 이메일 인증번호입니다.",
+      html: `
+    <h1>아래 링크를 클릭해주세요.</h1>
+    <a href="http://localhost:${process.env.PORT}/api/confirm/${res.locals.user.confirmationCode}">
+    이메일 인증번호 확인 링크
+    </a>      
+    `,
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  next();
+};
+
+const verifyUser = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      confirmationCode: req.params.confirmationCode,
+    });
+    if (!user) {
+      res.status(400).send(`이메일 인증에 실패했습니다.`);
+    } else {
+      user.status = "Active";
+      res.status(200).send(`이메일 인증이 완료되었습니다.`);
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
 const authenticateToken = (req, res, next) => {
   const user = User.findOne({
     email: req.body.email,
@@ -41,4 +85,6 @@ const authenticateToken = (req, res, next) => {
 module.exports = {
   generateConfirmationCode,
   authenticateToken,
+  sendEmailVerification,
+  verifyUser,
 };
