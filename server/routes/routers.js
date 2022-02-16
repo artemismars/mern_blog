8066f6ee9fc438b15c46cd77ba2c5e4d0b814c0d
artemismars <artemismars@outlook.com>
2022-02-16 09:22:29 +0900

    Fix: routers.js에 avatar생성기 미들웨어 추가
    `app.post('/signup')`에 `avatar.gitAvatarGenerator` 미들웨어 추가

--- server/routes/routers.js
+++ server/routes/routers.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth");
+const avatar = require("../middlewares/avatar");
 // router working
 router.post(
   "/signup",
   auth.generateConfirmationCode,
   auth.sendEmailVerification,
+  avatar.gitAvatarGenerator,
   require("./signup")
 );
 router.get("/confirm/:confirmationCode", auth.verifyUser);
