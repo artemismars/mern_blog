baf5a62b394a79eb4f79acb889e547e7db4e1e60
5witchkr <95848796+5witchkr@users.noreply.github.com>
2022-02-16 20:22:55 +0900

    Create content.js

---
+++ server/models/content.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+const contentSchema = mongoose.Schema({
+    username: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        maxlength: 50,
+        minlength: 1,
+        require: true
+    },
+    body: {
+        type: String,
+        require: true
+    },
+    File: {
+        type: String,
+    },
+    creatDate:{
+        type: Date,
+        default:Date.now
+    },
+},{ timestamps: true })
+
+
+const Content = mongoose.model('Content', contentSchema);
+
+module.exports = { Content }
