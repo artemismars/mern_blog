a2017edc02522b742f022963f2f1c52f2829259a
5witchkr <95848796+5witchkr@users.noreply.github.com>
2022-02-16 21:08:35 +0900

    Update contentup.js

--- server/routes/contentup.js
+++ server/routes/contentup.js
@@ -5,30 +5,7 @@ const { auth } = require("../middleware/auth");
 const multer = require("multer");
 
 
-router.post('/upload', async (req, res) => {
-    try {
-        if (!req.files) {
-            res.send({
-                status: false,
-                message: '파일 업로드 실패'
-            });
-        } else {
-            let f = req.files.uploadFile;
-            f.mv('./uploads/' + f.name);
-            res.send({
-                status: true,
-                message: '파일이 업로드 되었습니다.',
-                data: {
-                    name: f.name,
-                    minetype: f.minetype,
-                    size: f.size
-                }
-            });
-        }
-    } catch (err) {
-        res.status(500).send(err);
-    }
-})
+
 
 router.post("/publish", (req, res) => {
 
@@ -41,6 +18,9 @@ router.post("/publish", (req, res) => {
         })
     })
 });
+
+
+
 
 
 module.exports = router;
