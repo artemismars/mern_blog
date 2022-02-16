fed16f9760e40ee03793e481425ea09e6beff660
5witchkr <95848796+5witchkr@users.noreply.github.com>
2022-02-16 20:42:01 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,5 +27,4 @@ app.use(fileUpload({createParentPath: true}));
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use('/content', require('./routes/content'));
 app.use('/contentup', require('./routes/contentup'));
