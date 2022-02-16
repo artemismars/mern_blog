ac4b855d50bbc4f4f94521291342b0cf1f490014
artemismars <artemismars@outlook.com>
2022-02-16 21:33:40 +0900

    Fix server.js
    `server.js`에 미들웨어 수정했습니다.

--- server/server.js
+++ server/server.js
@@ -9,22 +9,17 @@ console.log(`${process.env.NODE_ENV} mode on`);
 
 const express = require("express");
 const app = express();
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
 const PORT = process.env.PORT;
 
+//미들웨어추가
+app.use(cors());
 app.use(express.json());
+app.use(fileUpload({ createParentPath: true }));
 app.use("/api", require("./routes/routers"));
+app.use("/contentup", require("./routes/contentup"));
 
 app.listen(PORT, () => {
   console.log(`listening to port on ${PORT}`);
 });
-
-const bodyParser = require('body-parser');
-const fileUpload = require('express-fileupload');
-const cors = require('cors');
-
-//미들웨어추가
-app.use(fileUpload({createParentPath: true}));
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use('/contentup', require('./routes/contentup'));
